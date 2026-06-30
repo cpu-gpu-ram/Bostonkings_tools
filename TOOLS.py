15ed0b1d1593bee4837353478df6b29c01075e17
cpu-gpu-ram <josephkerr110@gmail.com>
2026-06-30 17:02:46 -0500

    Added greet function and asscii art

--- TOOLS.py
+++ TOOLS.py
@@ -1,2 +1,35 @@
 TOOLS
 #Hello this is a summarey of all my tools this is a free to use resource
+
+
+
+#ALL NESSECARY PACKAGES
+import shlex
+import sys
+import os
+import subprocess
+import platform
+import getpass
+import pathlib
+import shutil
+import time
+import datetime
+import zipfile
+
+#import all the otther projects
+def Greeter():
+    print(r'
+    __  __
+  /  |/  | _  _
+ / /| /| |/ // /
+/_/ |_/  |_/  /
+         /__/
+   ______   ______   ______   __       ______
+  /_  __/  / __  /  / __  /  /_/      / ____/
+   / /    / /  / / / /  / / / /      _\___ \
+  / /    / /__/ / / /__/ / / /___   /____/ /
+ /_/    /______/ /______/ /_____/  /______/')
+    print(r'Welcome select the number that matchs the tool that u wish to use
+        before using this please read the read me for help in Py-Shell and any other tool type sos/help
+        check out my profile at: https://github.com/cpu-gpu-ram
+        emergency exit Ctrl c')
